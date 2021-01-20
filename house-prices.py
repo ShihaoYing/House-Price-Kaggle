a858d1f5213134e79f4f19e7c7155702668a5ff5
skyingsky@gmail.com <skyingsky@gmail.com>
2021-01-19 22:08:36 -0800

    preview data

---
+++ house-prices.py
@@ -0,0 +1,23 @@
+import os
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='darkgrid', palette='muted')
+from scipy import stats
+from scipy.stats import norm
+from scipy.stats import skew
+
+
+def load_data():
+    train = pd.read_csv('./house-prices-advanced-regression-techniques/train.csv')
+    test = pd.read_csv('./house-prices-advanced-regression-techniques/test.csv')
+    
+    print(train.head())
+    print(test.head())
+    return train, test
+
+
+load()
