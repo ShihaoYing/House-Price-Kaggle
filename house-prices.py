9dc4162d24c5ff134cd77709fe2a677e57b19064
skyingsky@gmail.com <skyingsky@gmail.com>
2021-01-23 22:34:54 -0800

    have fun - 2

--- house-prices.py
+++ house-prices.py
@@ -5,10 +5,11 @@ import seaborn as sns
 
 
 train = pd.read_csv('./house-prices-advanced-regression-techniques/train.csv')
-test = pd.read_csv('./house-prices-advanced-regression-techniques/test.csv')   
+test = pd.read_csv('./house-prices-advanced-regression-techniques/test.csv')
 
 # print(train.head())
 # print(test.head())
+
 
 def sale_distribution():
     sns.displot(train['SalePrice'])
@@ -17,6 +18,7 @@ def sale_distribution():
 # print(train['SalePrice'].describe())
 # print("偏度: %f" % train['SalePrice'].skew())
 # print("峰度: %f" % train['SalePrice'].kurt())
+
 
 data = train.corr()
 print(data['SalePrice'].sort_values())
